6a3634b52fa5ff99a744c87ed412fc11535a314e
Michael <15317421+ActualMandM@users.noreply.github.com>
2022-05-28 16:49:04 -0700

    [BypassSaturationBlock] Switch to signature scanning.

--- Source/BypassSaturationBlock/Pch.h
+++ Source/BypassSaturationBlock/Pch.h
@@ -2,10 +2,15 @@
 
 #define WIN32_LEAN_AND_MEAN
 
+// Detours
 #include <Windows.h>
 #include <detours.h>
 
+// Standard library
 #include <cstdint>
 #include <cstdio>
+#include <string>
 
-#include "Helpers.h"
+// Dependencies
+#include <Helpers.h>
+#include <Signature.h>
